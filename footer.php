1931a5eca1ab6166e05f45be60a80ffd813e24b2
Zakir Sajib <zakirsajib@gmail.com>
2019-04-08 15:23:13 +0100

    no message

--- footer.php
+++ footer.php
@@ -38,7 +38,7 @@ $container = get_theme_mod( 'understrap_container_type' );
 						<li><a href="#">Option 03</a></li>
 					</ul>
 				</div>
-				<div class="col-6 col-lg-3 col-xl-3 d-none d-sm-block d-md-block d-lg-block d-xl-block">
+				<div class="col-6 col-lg-3 d-none d-sm-block">
 					<ul>
 						<li><h5>Menu 02</h5></li>
 						<li><a href="#">Option 01</a></li>
@@ -46,7 +46,7 @@ $container = get_theme_mod( 'understrap_container_type' );
 						<li><a href="#">Option 03</a></li>
 					</ul>
 				</div>
-				<div class="col-6 col-lg-3 col-xl-3 d-none d-sm-block d-md-block d-lg-block d-xl-block">
+				<div class="col-6 col-lg-3 d-none d-sm-block">
 					<ul>
 						<li><h5>Menu 03</h5></li>
 						<li><a href="#">Option 01</a></li>
@@ -84,7 +84,7 @@ $container = get_theme_mod( 'understrap_container_type' );
 <div class="social-footer">	
 	<div class="<?php echo esc_attr( $container ); ?>">
 		<div class="row justify-content-center">
-			<div class="align-items-center d-none d-sm-block d-md-block d-lg-block d-xl-block">
+			<div class="align-items-center d-none d-sm-block">
 				<div class="d-flex">
 					<div class="flex-fill"><a href="#" target="_blank"><i class="pt pt-facebook pt-lg"></i> Facebook</a></div>
 					<div class="flex-fill"><a href="#" target="_blank"><i class="pt pt-instagram pt-lg"></i> Instagram</a></div>
@@ -92,7 +92,7 @@ $container = get_theme_mod( 'understrap_container_type' );
 					<div class="flex-fill"><a href="#" target="_blank"><i class="pt pt-envelope pt-lg"></i> Email</a></div>
 				</div>
 			</div>
-			<div class="align-items-center d-block d-xl-none d-lg-none d-md-none">
+			<div class="align-items-center d-block d-md-none">
 				<div class="d-flex">
 					<div class="flex-fill"><a href="#" target="_blank"><i class="pt pt-facebook pt-2x"></i></a></div>
 					<div class="flex-fill"><a href="#" target="_blank"><i class="pt pt-instagram pt-2x"></i></a></div>
