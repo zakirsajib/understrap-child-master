93488e6f2943461d7b1ddb3367fd7e3dfaefdba8
Zakir Sajib <zakirsajib@gmail.com>
2019-04-09 15:08:16 +0100

    no message

--- inc/hooks.php
+++ inc/hooks.php
@@ -26,7 +26,7 @@ if ( ! function_exists( 'understrap_add_site_info' ) ) {
 	 */
 	function understrap_add_site_info() {
 		$current_year = date('Y');
-		$site_info = sprintf('&copy; %u. All rights reserved.', $current_year );
+		$site_info = sprintf('&copy; 2018 - %u. All rights reserved.', $current_year );
 
 		echo apply_filters( 'understrap_site_info_content', $site_info ); // WPCS: XSS ok.
 	}
